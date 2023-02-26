f5b78e90617303ba776466ee4a83dec382d6c7f1
Laurent Fenu <agranat.lf@gmail.com>
2023-02-26 14:10:00 +0100

    react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from './pages/Home'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
